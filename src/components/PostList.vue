f00ae58be254920b75b6d555175e1452cbcf4ff5
catalin.festila <catafest76@gmail.com>
2022-05-06 12:02:20 +0300

    create posts

---
+++ src/components/PostList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="post-list">
+    <!-- set template from each post with v-for based on Home.vue -->
+    <div v-for="post in posts" :key="post.id">
+      <!-- <h3>{{ post.title }}</h3> -->
+      <SinglePost :post="post" />
+    </div> 
+  </div>
+</template>
+<script>
+import { onMounted , onUnmounted, onUpdated } from 'vue'
+import SinglePost from './SinglePost.vue'
+export default {
+  props: ['posts'], 
+  components: { SinglePost },
+  // export the posts 
+  // using the 'setup' with 'this'
+  setup(props) {
+    //console.log(props);
+    //console.log(props.posts);
+    onMounted(() => console.log('component mounted'))
+    onUnmounted(() => console.log('component unmounted'))
+    onUpdated(() => console.log('component updated'))
+  }//,
+  // mounted() {
+  //   console.log('mounted using options api');
+  // },
+  // mounted() {
+
+  // },
+  // updated() {
+
+  // }
+}
+</script>
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <PostList v-if="showPosts" :posts="posts"  />
+    <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">delete a post</button>
+  </div>
+</template>
+<script>
+import PostList from '../components/PostList.vue'
+import {ref} from 'vue'
+export default {
+  name: 'Home',
+  components: { PostList},
+  setup() {
+    const posts = ref([
+      { title: 'welcome to the blog ', body: 'Lorem ipsum', id: 1 },
+      { title: 'top 5 CSS tips', body: 'lorem ipsum', id: 2},
+    ])
+    const showPosts = ref(true)
+    return { posts, showPosts }
+  }
+}
+</script>
+
+<style>
+</style>
